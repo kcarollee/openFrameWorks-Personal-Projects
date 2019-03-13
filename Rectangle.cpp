040bf303b0b97bc9795502765b4460a2932f1a56
Dukwon Karl Lee (이덕원) <43235659+kcarollee@users.noreply.github.com>
2019-03-13 18:28:53 +0900

    some new edits

---
+++ Rectangle.cpp
@@ -0,0 +1,96 @@
+#include "Rectangle.h"
+vector<int> Block::schedule;
+vector<vector<int>> Block::scheduleArr;
+Block::Block()
+{
+
+
+}
+Block::~Block()
+{
+	ofRemoveListener(ofEvents().mouseMoved, this, &Block::mouseMovedHandler);
+	ofRemoveListener(ofEvents().mousePressed, this, &Block::mousePressedHandler);
+}
+
+void Block::setup(int x, int y, int w, int h)
+{
+	posX = x;
+	posY = y;
+	width = w;
+	height = h;
+
+	blockID = 0;
+
+	color = ofColor(255, 255, 255);
+
+	rect.set(posX, posY, width, height);
+
+	pressedFlag = false;
+
+	ofAddListener(ofEvents().mouseMoved, this, &Block::mouseMovedHandler);
+	ofAddListener(ofEvents().mousePressed, this, &Block::mousePressedHandler);
+
+}
+void Block::reset()
+{
+	color = ofColor(255, 255, 255);
+	pressedFlag = false;
+}
+void Block::draw()
+{
+	ofSetColor(color);
+	ofDrawRectangle(rect);
+
+}
+
+bool Block::isInside(int x, int y)
+{
+	if ((x > rect.x) && (x < rect.x + rect.getWidth())) {
+		if ((y > rect.y) && (y < rect.y + rect.getHeight())) {
+
+			return true;
+		}
+	}
+
+	return false;
+}
+void Block::mouseMovedHandler(ofMouseEventArgs & mouse)
+{
+	if (isInside(mouse.x, mouse.y)) {
+		//cout << "in block number " << blockID << endl;
+		color = ofColor(255, 0, 0);
+	}
+
+	else {
+		if (!pressedFlag) {
+			color = ofColor(255, 255, 255);
+		}
+		else
+			color = ofColor(80, 80, 80);
+	}
+
+
+}
+void Block::mousePressedHandler(ofMouseEventArgs & mouse)
+{
+	if (isInside(mouse.x, mouse.y)) {
+		if (!pressedFlag) {
+			pressedFlag = true;
+			schedule.push_back(blockID);
+			for (auto i : schedule) {
+				cout << i << " " << endl;
+			}
+		}
+		else {
+
+			pressedFlag = false;
+			for (int i = 0; i < schedule.size(); i++) {
+				if (schedule[i] == blockID) {
+					schedule.erase(schedule.begin() + i);
+				}
+			}
+		}
+	}
+
+
+}
---
+++ ofApp.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Rectangle.h"
+
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+	void Clear();
+	void Compare();
+	void setupInitial();
+	bool isInsideButton(int x, int y, ofRectangle rect);
+
+public:
+	vector<Block*> rectArr;
+
+	ofRectangle registerButton;
+	ofRectangle resultsButton;
+	ofRectangle allClearButton;
+	ofRectangle howToBox;
+
+	string recordStatus;
+
+	vector<int> initialAvail;
+	vector<string> days = { "MON", "TUE", "WED", "THUR", "FRI" };
+	vector<string> period = { "1st", "2nd", "3rd", "4th", "5th",
+								"6th", "7th", "8th", "9th" };
+
+	ofTrueTypeFont font;
+	ofTrueTypeFont smallFont;
+
+	ofTrueTypeFont howToGuide;
+
+	int blockID;
+
+	ofColor registerColor;
+	ofColor resultsColor;
+	ofColor clearColor;
+	ofColor howToColor;
+
+
+
+};
